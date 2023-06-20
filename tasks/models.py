6af62f61e7ee8cc848ffcfad89832d3d90ce9190
aamrlyman <aamrlyman@gmail.com>
2023-06-20 08:24:49 -0700

    add blank=true and defaults to models.py

--- tasks/models.py
+++ tasks/models.py
@@ -9,9 +9,9 @@ class Task(models.Model):
         ('completed', 'Completed'),
     ]   
     title = models.CharField(max_length=300)
-    due_date = models.DateField()
-    status = models.CharField(max_length=20, choices=TASK_STATUS_CHOICES)
-    description = models.CharField(max_length=1000)
+    due_date = models.DateField(blank=True)
+    status = models.CharField(max_length=20, choices=TASK_STATUS_CHOICES, default = 'ready')
+    description = models.CharField(max_length=1000, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
  
 # Create your models here.
