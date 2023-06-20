1effee4c376d1c81069c017176cfa65ac2676171
aamrlyman <aamrlyman@gmail.com>
2023-06-20 08:37:18 -0700

    update date field w/null=True

--- tasks/models.py
+++ tasks/models.py
@@ -9,7 +9,7 @@ class Task(models.Model):
         ('completed', 'Completed'),
     ]   
     title = models.CharField(max_length=300)
-    due_date = models.DateField(blank=True)
+    due_date = models.DateField(null=True)
     status = models.CharField(max_length=20, choices=TASK_STATUS_CHOICES, default = 'ready')
     description = models.CharField(max_length=1000, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
